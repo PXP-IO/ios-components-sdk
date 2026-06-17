ccd31a537854d93e91e5e93a997540a665cdfe57
hoanphanq <hoangphanq@nashtechglobal.com>
2026-06-17 22:28:59 +0700

    update frameworks

--- Package.swift
+++ Package.swift
@@ -1,6 +1,20 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
+let sdkBinaryDependencies: [Target.Dependency] = [
+    "PXPCheckoutSDK",
+    "KountDataCollector",
+    "Alamofire",
+    "Swinject",
+    "CorePayments",
+    "PaymentButtons",
+    "PayPalWebPayments",
+    "CardPayments",
+    "FraudProtection",
+    "PPRiskMagnes",
+    "aerosync_ios_sdk"
+]
+
 let package = Package(
     name: "PXPCheckout",
     platforms: [
@@ -10,23 +24,18 @@ let package = Package(
         .library(
             name: "PXPCheckout",
             targets: ["PXPCheckout"]
+        ),
+        .library(
+            name: "PXPCheckoutSDK",
+            targets: ["PXPCheckoutSDK"]
         )
     ],
     dependencies: [],
     targets: [
         .target(
             name: "PXPCheckout",
-            dependencies: [
-                "PXPCheckoutSDK",
-                "KountDataCollector",
-                "Alamofire",
-                "CorePayments",
-                "PaymentButtons",
-                "PayPalWebPayments",
-                "CardPayments",
-                "FraudProtection",
-                "PPRiskMagnes"
-            ]
+            dependencies: sdkBinaryDependencies,
+            path: "Sources/PXPCheckout"
         ),
         .binaryTarget(
             name: "PXPCheckoutSDK",
@@ -35,6 +44,10 @@ let package = Package(
         .binaryTarget(
             name: "Alamofire",
             path: "Frameworks/Alamofire.xcframework"
+        ),
+        .binaryTarget(
+            name: "Swinject",
+            path: "Frameworks/Swinject.xcframework"
         ),
         .binaryTarget(
             name: "CorePayments",
@@ -63,7 +76,10 @@ let package = Package(
         .binaryTarget(
             name: "KountDataCollector",
             path: "Frameworks/KountDataCollector.xcframework"
+        ),
+        .binaryTarget(
+            name: "aerosync_ios_sdk",
+            path: "Frameworks/aerosync_ios_sdk.xcframework"
         )
     ]
 )
-
---
+++ Sources/PXPCheckoutSDKExport/PXPCheckoutSDKExport.swift
@@ -0,0 +1,7 @@
+//
+// PXPCheckoutSDKExport.swift
+// PXP Checkout iOS SDK
+//
+
+/// Re-export PXPCheckoutSDK functionality for merchants using the PXPCheckoutSDK product.
+@_exported import PXPCheckoutSDK
