d8d960c90f3f7c51ddc435f9c9f165cee15f1027
Azure Pipeline <azure-pipeline@pxp.com>
2026-07-28 05:40:44 +0000

    Update iOS Package - v1.2.1
    - Updated XCFrameworks (11 frameworks)
    - Updated Package.swift manifest
    - Updated Sources/PXPCheckout wrapper
    - Updated README.md
    
    Build: 20260728.2
    Version: 1.2.1

--- Sources/PXPCheckout/PXPCheckout.swift
+++ Sources/PXPCheckout/PXPCheckout.swift
@@ -7,7 +7,7 @@
 
 // Swift Package Manager target for PXPCheckout
 
-/// Re-export PXPCheckoutSDK functionality
-@_exported import PXPCheckoutSDK
 /// Re-export KountDataCollector functionality
 @_exported import KountDataCollector
+/// Re-export PXPCheckoutSDK functionality
+@_exported import PXPCheckoutSDK
